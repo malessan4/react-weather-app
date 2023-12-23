1e4c07524d8fc7089a818cc8c56e76912b28f2f2
Matias-Alessandrello <matialess@gmail.com>
2023-12-23 19:39:46 -0300

    segunda secion

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,29 +1,40 @@
+import {useState} from "react"
 
 export const WeatherApp = () => {
 
-  const [ciudad, setCiudad] = useState()
-  const [dataClima, setDataClima] = useState(second)
+  const urlBase = `https://api.openweathermap.org/data/2.5/weather`
+  const API_KEY = 'a3961611948a9cd2b1c692920676a458'
 
-  const handleCambioCiudad = ()=>{
+  const [ciudad, setCiudad] = useState('')
+  const [dataClima, setDataClima] = useState(null)
+
+  const handleCambioCiudad = (e)=> {
     setCiudad(e.target.value)
   }
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
     if(ciudad.length > 0) fetchClima()
   }
 
-
+  const fetchClima = async () => {
+    try{
+      const response = await fetch (`${urlBase}?=${ciudad}&appid=${API_KEY}`)
+      const data = await response.json()
+      setDataClima(data)
+  }catch(error){
+    console.error(error)
+  }
+}
   return (
     <div className="container">
-      <h1>Aplicacion de clima</h1>
+      <h1>Aplicación de clima</h1>
 
       <form onSubmit={handleSubmit}>
         <input
         type="text"
         value={ciudad}
         onChange={handleCambioCiudad}
-        
         />
         <button type="submit">Buscar</button>
       </form>
